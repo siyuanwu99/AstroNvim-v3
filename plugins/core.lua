8f32dae25b21e0fd561fdb3d93cbe7f4a3938a01
edmundwsy <edmundwsy@outlook.com>
2023-06-11 10:33:04 +0200

    refine alpha page

--- plugins/core.lua
+++ plugins/core.lua
@@ -17,6 +17,18 @@ return {
         "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
         "    ██   ████   ████   ██ ██      ██",
       }
+
+      local button = require("astronvim.utils").alpha_button
+      local get_icon = require("astronvim.utils").get_icon
+      opts.section.buttons.val = {
+        button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+        button("LDR f p", get_icon("Package", 2, true) .. "Projects"),
+        button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+        button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+        button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+        button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+        button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      }
       return opts
     end,
   },
