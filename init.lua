5a46d9e5f831c07c53abdbd2c9f65e83da418148
Siyuan Wu <edmundwsy@outlook.com>
2023-12-01 13:15:49 +0800

    sync

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "onedark",
+  colorscheme = "gruvbox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
